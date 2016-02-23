51b06b1e57f47ad8ee573f12ac7a30ca75755674
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-02-23 16:13:19 -0500

    Playthrough entity in .xcdatamodeld has new attributes for Questions answered and the specific answers potentially chosen

--- DreamGame/DreamGame/Playthrough+CoreDataProperties.h
+++ DreamGame/DreamGame/Playthrough+CoreDataProperties.h
@@ -2,7 +2,7 @@
 //  Playthrough+CoreDataProperties.h
 //  DreamGame
 //
-//  Created by Amy Joscelyn on 2/22/16.
+//  Created by Amy Joscelyn on 2/23/16.
 //  Copyright © 2016 Amy Joscelyn. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,6 +16,25 @@ NS_ASSUME_NONNULL_BEGIN
 @interface Playthrough (CoreDataProperties)
 
 @property (nonatomic) BOOL fontChange;
+@property (nullable, nonatomic, retain) NSString *answerQ3;
+@property (nullable, nonatomic, retain) NSString *answerQ3A;
+@property (nullable, nonatomic, retain) NSString *answerQ3B;
+@property (nullable, nonatomic, retain) NSString *answerQ7;
+@property (nullable, nonatomic, retain) NSString *answerQ7A;
+@property (nonatomic) BOOL creativityChosen;
+@property (nonatomic) BOOL intelligenceChosen;
+@property (nonatomic) BOOL obedienceChosen;
+@property (nonatomic) BOOL empathyChosen;
+@property (nonatomic) BOOL instinctChosen;
+@property (nonatomic) BOOL perseveranceChosen;
+@property (nonatomic) BOOL kindnessChosen;
+@property (nonatomic) BOOL strengthChosen;
+@property (nonatomic) BOOL graceChosen;
+@property (nonatomic) BOOL intellectChosen;
+@property (nonatomic) BOOL imaginationChosen;
+@property (nonatomic) BOOL caringChosen;
+@property (nonatomic) BOOL wondermentChosen;
+@property (nonatomic) BOOL curiosityChosen;
 @property (nullable, nonatomic, retain) Question *currentQuestion;
 @property (nullable, nonatomic, retain) Character *playerCharacter;
 
--- DreamGame/DreamGame/Playthrough+CoreDataProperties.m
+++ DreamGame/DreamGame/Playthrough+CoreDataProperties.m
@@ -2,7 +2,7 @@
 //  Playthrough+CoreDataProperties.m
 //  DreamGame
 //
-//  Created by Amy Joscelyn on 2/22/16.
+//  Created by Amy Joscelyn on 2/23/16.
 //  Copyright © 2016 Amy Joscelyn. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,6 +14,25 @@
 @implementation Playthrough (CoreDataProperties)
 
 @dynamic fontChange;
+@dynamic answerQ3;
+@dynamic answerQ3A;
+@dynamic answerQ3B;
+@dynamic answerQ7;
+@dynamic answerQ7A;
+@dynamic creativityChosen;
+@dynamic intelligenceChosen;
+@dynamic obedienceChosen;
+@dynamic empathyChosen;
+@dynamic instinctChosen;
+@dynamic perseveranceChosen;
+@dynamic kindnessChosen;
+@dynamic strengthChosen;
+@dynamic graceChosen;
+@dynamic intellectChosen;
+@dynamic imaginationChosen;
+@dynamic caringChosen;
+@dynamic wondermentChosen;
+@dynamic curiosityChosen;
 @dynamic currentQuestion;
 @dynamic playerCharacter;
 
