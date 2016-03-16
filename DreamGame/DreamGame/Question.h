c2bc13445faa054db38bd6df422b9bdb4efb17ff
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-03-16 16:12:33 -0400

    added back forward declarations that had been mistakenly deleted

--- DreamGame/DreamGame/Question.h
+++ DreamGame/DreamGame/Question.h
@@ -10,7 +10,7 @@
 #import <CoreData/CoreData.h>
 #import <CHCSVParser/CHCSVParser.h> //do I need this imported here?
 
-@class Choice;
+@class Choice, Effect, Playthrough, Prerequisite;
 
 NS_ASSUME_NONNULL_BEGIN
 
